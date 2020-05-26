503dbd610d22554f8abd09dd3b02c9320eac89d4
fabio.nascimento <fabio.nascimento@capgemini.com>
2020-05-26 18:54:16 -0300

    commit

---
+++ info.sql
@@ -0,0 +1,6 @@
+
+
+
+git --version - OK
+git config --global user.name "Fabio Nascimento" && git config --global user.email "bimnascimento@gmail.com"
+git clone https://github.com/devexamemail/treinamentojavaweb.git
