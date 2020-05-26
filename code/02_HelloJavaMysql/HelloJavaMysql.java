7d772a4f07d2512f8eedaafb52761702b1d8f700
fabio.nascimento <fabio.nascimento@capgemini.com>
2020-05-26 19:56:58 -0300

    mysql

--- code/02_HelloJavaMysql/HelloJavaMysql.java
+++ code/02_HelloJavaMysql/HelloJavaMysql.java
@@ -7,8 +7,33 @@ import java.sql.PreparedStatement;
 
 public class HelloJavaMysql {
     public static void main(String[] args) {
-        System.out.println("------------------------------------------------");
-        System.out.println("HelloJavaMysql");
-        System.out.println("------------------------------------------------");
+        try {
+			
+			String dbDriver = "com.mysql.cj.jdbc.Driver"; 
+            String dbURL = "jdbc:mysql://191.252.0.230:3306/"; 
+            String dbName = "cap?useSSL=false"; 
+            String dbUsername = "root"; 
+            String dbPassword = ""; 
+			Class.forName(dbDriver);
+			Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword); 
+
+			Statement st = con.createStatement();
+			ResultSet result = st.executeQuery("SELECT * FROM funcionarios");
+			System.out.println("------------------------------------------------");
+	        System.out.println("Funcionario \n");
+	        int count = 1;
+	        while(result.next()){
+	            System.out.println(count++ + " ) Nome: " + result.getString("nome") + " / cel: " + result.getString("celular")  );
+			}
+			st.close();
+			System.out.println("------------------------------------------------");
+			
+			
+			// close
+			con.close();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} 
     }
 }
