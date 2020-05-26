4b95e00527d790684c7d8613ff35cc49dba8591b
fabio.nascimento <fabio.nascimento@capgemini.com>
2020-05-26 20:18:58 -0300

    mysql

--- code/02_HelloJavaMysql/HelloJavaMysql.java
+++ code/02_HelloJavaMysql/HelloJavaMysql.java
@@ -10,14 +10,28 @@ public class HelloJavaMysql {
         try {
 			
 			String dbDriver = "com.mysql.cj.jdbc.Driver"; 
-            String dbURL = "jdbc:mysql://191.252.0.230:3306/"; 
+            String dbURL = "jdbc:mysql://localhost:3306/"; 
             String dbName = "cap?useSSL=false"; 
             String dbUsername = "root"; 
             String dbPassword = ""; 
 			Class.forName(dbDriver);
 			Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword); 
 
-			Statement st = con.createStatement();
+			int cadastro = 1;
+			// Cadastrar
+			if(cadastro == 1) {
+				PreparedStatement pst = con.prepareStatement("INSERT INTO funcionarios (`celular`, `cliente_alocado`, `email`, `funcao`, `nome`, `usuario_github`) VALUES (?,?,?,?,?,?) ");
+				pst.setString(1, "1198888885"); // Celular
+				pst.setString(2, "Cliente Alocado"); // Cliente Alocado
+				pst.setString(3, "Email"); // Email
+				pst.setString(4, "Funcao"); // Função dentro da CAP
+				pst.setString(5, "Nome"); // Nome Completo
+				pst.setString(6, "Usuario GitHub"); // Usuario do Github
+				pst.executeUpdate();
+				pst.close();
+			}
+
+	        Statement st = con.createStatement();
 			ResultSet result = st.executeQuery("SELECT * FROM funcionarios");
 			System.out.println("------------------------------------------------");
 	        System.out.println("Funcionario \n");
