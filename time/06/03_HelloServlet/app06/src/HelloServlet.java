7c66608326cd2612d1b2ebb843099948c31b5c91
Jorge Nakamura <jorge.nakamura@capgemini.com>
2020-05-28 18:12:25 -0300

    commit

---
+++ time/06/03_HelloServlet/app06/src/HelloServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		PrintWriter writer = res.getWriter();
+		writer.println("<html><head><title>Hello Servlet</title></head>");
+		writer.println("<body><h1>Hello Servlet</h1>");
+		writer.println("</body></html>");
+	}
+}
