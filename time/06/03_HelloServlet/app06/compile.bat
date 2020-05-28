c35b3e3f3fe51125f1f01045f0deef499f54e384
Jorge Nakamura <jorge.nakamura@capgemini.com>
2020-05-28 18:44:20 -0300

    commit

---
+++ time/06/03_HelloServlet/app06/compile.bat
@@ -0,0 +1,17 @@
+@echo off
+SET _JAVA_OPTIONS=
+
+
+echo -----------------------
+set app=app06
+echo compilando %app% ...
+javac -classpath "D:\Cursos\JAVA CAP\Workspace\treinamentojavaweb\code\workspace\servidor\apache-tomcat-9.0.35\lib\servlet-api.jar" %app%\src\HelloServlet.java
+echo criando estrutura de pasta ...
+copy %app%\src\HelloServlet.class %app%\WEB-INF\classes
+del %app%\src\HelloServlet.class
+echo gerando arquivo war ...
+cmd /c "cd %app% & jar cf ..\%app%.war *"
+echo arquivo %app%.war gerado com sucesso!!
+
+
+
