51d960cb77425f3ba5c8c4805d00a6abeb7d4da2
fabio.nascimento <fabio.nascimento@capgemini.com>
2020-05-28 18:44:09 -0300

    tomcat

--- code/03_HelloServlet/compile.bat
+++ code/03_HelloServlet/compile.bat
@@ -1,11 +1,10 @@
 @echo off
 SET _JAVA_OPTIONS=
-set CATALINA_HOME "C:\WORK\treinamentojavaweb\code\apache-tomcat-9.0.35" /m 
 
 echo -----------------------
 set app=app01
 echo compilando %app% ...
-javac -classpath "%CATALINA_HOME%\lib\servlet-api.jar" %app%\src\HelloServlet.java
+javac -classpath "C:\WORK\treinamentojavaweb\code\workspace\servidor\apache-tomcat-9.0.35\lib\servlet-api.jar" %app%\src\HelloServlet.java
 echo criando estrutura de pasta ...
 copy %app%\src\HelloServlet.class %app%\WEB-INF\classes
 del %app%\src\HelloServlet.class
@@ -17,7 +16,7 @@ echo arquivo %app%.war gerado com sucesso!!
 echo -----------------------
 set app=app02
 echo compilando %app% ...
-javac -classpath "%CATALINA_HOME%\lib\servlet-api.jar" %app%\src\HelloServlet.java
+javac -classpath "C:\WORK\treinamentojavaweb\code\workspace\servidor\apache-tomcat-9.0.35\lib\servlet-api.jar" %app%\src\HelloServlet.java
 echo criando estrutura de pasta ...
 copy %app%\src\HelloServlet.class %app%\WEB-INF\classes
 del %app%\src\HelloServlet.class
